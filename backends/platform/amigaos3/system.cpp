48c37173a853311a2896e05cfdebc2a22e64a7b1
Robin Southern <betajaen@users.noreply.github.com>
2022-01-28 07:14:34 +0000

    Prototype of AmigaOS3System

--- backends/platform/amigaos3/system.cpp
+++ backends/platform/amigaos3/system.cpp
@@ -21,3 +21,200 @@
  */
 
 #include "system.h"
+
+
+AmigaOS3System::AmigaOS3System() {
+}
+
+AmigaOS3System::~AmigaOS3System() {
+}
+
+void AmigaOS3System::init() {
+}
+
+void AmigaOS3System::initBackend() {
+}
+
+void AmigaOS3System::engineInit() {
+}
+
+void AmigaOS3System::engineDone() {
+}
+
+bool AmigaOS3System::hasFeature() {
+	return false;
+}
+
+void AmigaOS3System::setFeatureState() {
+}
+
+bool AmigaOS3System::getFeatureState() {
+	return false;
+}
+
+const GraphicsMode *AmigaOS3System::getSupportedGraphicsModes() const {
+	return NULL;
+}
+
+int AmigaOS3System::getDefaultGraphicsMode() const {
+	return 0;
+}
+
+bool AmigaOS3System::setGraphicsMode(int mode, uint flags) {
+	return false;
+}
+
+int AmigaOS3System::getGraphicsMode() const {
+	return 0;
+}
+
+void AmigaOS3System::initSize(uint width, uint height, const Graphics::PixelFormat *format) {
+}
+
+void AmigaOS3System::initSizeHint(const Graphics::ModeList &modes) {
+}
+
+int AmigaOS3System::getScreenChangeID() const {
+	return 0;
+}
+
+void AmigaOS3System::beginGFXTransaction() {
+}
+
+TransactionError AmigaOS3System::endGFXTransaction() {
+	return kTransactionFormatNotSupported;
+}
+
+int16 AmigaOS3System::getHeight() {
+	return 0;
+}
+
+int16 AmigaOS3System::getWidth() {
+	return 0;
+}
+
+PaletteManager *AmigaOS3System::getPaletteManager() {
+	return nullptr;
+}
+
+void AmigaOS3System::copyRectToScreen(const void *buf, int pitch, int x, int y, int w, int h) {
+}
+
+Graphics::Surface *AmigaOS3System::lockScreen() {
+	return NULL;
+}
+
+void AmigaOS3System::unlockScreen() {
+}
+
+void AmigaOS3System::fillScreen(uint32 col) {
+}
+
+void AmigaOS3System::updateScreen() {
+}
+
+void AmigaOS3System::setShakePos(int shakeXOffset, int shakeYOffset) {
+}
+
+void AmigaOS3System::setFocusRectangle(const Common::Rect &rect) {
+}
+
+void AmigaOS3System::clearFocusRectangle() {
+}
+
+void AmigaOS3System::showOverlay() {
+}
+
+void AmigaOS3System::hideOverlay() {
+}
+
+bool AmigaOS3System::isOverlayVisible() const {
+	return false;
+}
+
+Graphics::PixelFormat AmigaOS3System::getOverlayFormat() const {
+	return Graphics::PixelFormat();
+}
+
+void AmigaOS3System::clearOverlay() {
+}
+
+void AmigaOS3System::grabOverlay(Graphics::Surface &surface) {
+}
+
+void AmigaOS3System::copyRectToOverlay(const void *buf, int pitch, int x, int y, int w, int h) {
+}
+
+int16 AmigaOS3System::getOverlayHeight() {
+	return int16();
+}
+
+int16 AmigaOS3System::getOverlayWidth() {
+	return int16();
+}
+
+bool AmigaOS3System::showMouse(bool visible) {
+	return false;
+}
+
+bool AmigaOS3System::lockMouse(bool lock) {
+	return false;
+}
+
+void AmigaOS3System::warpMouse(int x, int y) {
+}
+
+void AmigaOS3System::setMouseCursor(const void *buf, uint w, uint h, int hotspotX, int hotspotY, uint32 keycolor, bool dontScale, const Graphics::PixelFormat *format) {
+}
+
+void AmigaOS3System::setCursorPalette(const byte *colors, uint start, uint num) {
+}
+
+uint32 AmigaOS3System::getMillis(bool skipRecord) {
+	return uint32();
+}
+
+void AmigaOS3System::delayMillis(uint msecs) {
+}
+
+void AmigaOS3System::getTimeAndDate(TimeDate &td, bool skipRecord) const {
+}
+
+MutexRef AmigaOS3System::createMutex() {
+	return MutexRef();
+}
+
+void AmigaOS3System::lockMutex(MutexRef mutex) {
+}
+
+void AmigaOS3System::unlockMutex(MutexRef mutex) {
+}
+
+void AmigaOS3System::deleteMutex(MutexRef mutex) {
+}
+
+Audio::Mixer *AmigaOS3System::getMixer() {
+	return nullptr;
+}
+
+void AmigaOS3System::quit() {
+}
+
+void AmigaOS3System::fatalError() {
+}
+
+void AmigaOS3System::displayMessageOnOSD(const Common::U32String &msg) {
+}
+
+void AmigaOS3System::displayActivityIconOnOSD(const Graphics::Surface *icon) {
+}
+
+void AmigaOS3System::logMessage(LogMessageType::Type type, const char *message) {
+}
+
+void AmigaOS3System::messageBox(LogMessageType::Type type, const char *message) {
+}
+
+bool AmigaOS3System::isConnectionLimited() {
+	return false;
+}
--- backends/platform/amigaos3/system.h
+++ backends/platform/amigaos3/system.h
@@ -23,4 +23,126 @@
 #ifndef AMIGAOS3_SYSTEM_H
 #define AMIGAOS3_SYSTEM_H
 
+#include "common/system.h"
+
+class AmigaOS3System : public OSystem {
+
+public:
+	AmigaOS3System();
+
+	virtual ~AmigaOS3System();
+
+	virtual void init() override;
+
+	virtual void initBackend() override;
+
+	virtual void engineInit() override;
+
+	virtual void engineDone() override;
+
+	virtual bool hasFeature() override;
+
+	virtual void setFeatureState() override;
+
+	virtual bool getFeatureState() override;
+
+	virtual const GraphicsMode *getSupportedGraphicsModes() const override;
+
+	virtual int getDefaultGraphicsMode() const override;
+
+	virtual bool setGraphicsMode(int mode, uint flags) override;
+
+	virtual int getGraphicsMode() const override;
+
+	virtual void initSize(uint width, uint height, const Graphics::PixelFormat *format) override;
+
+	virtual void initSizeHint(const Graphics::ModeList &modes) override;
+
+	virtual int getScreenChangeID() const override;
+
+	virtual void beginGFXTransaction() override;
+
+	virtual TransactionError endGFXTransaction() override;
+
+	virtual int16 getHeight() override;
+
+	virtual int16 getWidth() override;
+
+	virtual PaletteManager *getPaletteManager() override;
+
+	virtual void copyRectToScreen(const void *buf, int pitch, int x, int y, int w, int h) override;
+
+	virtual Graphics::Surface *lockScreen() override;
+
+	virtual void unlockScreen() override;
+
+	virtual void fillScreen(uint32 col) override;
+
+	virtual void updateScreen() override;
+
+	virtual void setShakePos(int shakeXOffset, int shakeYOffset) override;
+
+	virtual void setFocusRectangle(const Common::Rect &rect) override;
+
+	virtual void clearFocusRectangle() override;
+
+	virtual void showOverlay() override;
+
+	virtual void hideOverlay() override;
+
+	virtual bool isOverlayVisible() const override;
+
+	virtual Graphics::PixelFormat getOverlayFormat() const override;
+
+	virtual void clearOverlay() override;
+
+	virtual void grabOverlay(Graphics::Surface &surface) override;
+
+	virtual void copyRectToOverlay(const void *buf, int pitch, int x, int y, int w, int h) override;
+
+	virtual int16 getOverlayHeight() override;
+
+	virtual int16 getOverlayWidth() override;
+
+	virtual bool showMouse(bool visible) override;
+
+	virtual bool lockMouse(bool lock) override;
+
+	virtual void warpMouse(int x, int y) override;
+
+	virtual void setMouseCursor(const void *buf, uint w, uint h, int hotspotX, int hotspotY, uint32 keycolor, bool dontScale = false, const Graphics::PixelFormat *format = nullptr) override;
+
+	virtual void setCursorPalette(const byte *colors, uint start, uint num) override;
+
+	virtual uint32 getMillis(bool skipRecord = false) override;
+
+	virtual void delayMillis(uint msecs) override;
+
+	virtual void getTimeAndDate(TimeDate &td, bool skipRecord = false) const override;
+
+	virtual MutexRef createMutex() override;
+
+	virtual void lockMutex(MutexRef mutex) override;
+
+	virtual void unlockMutex(MutexRef mutex) override;
+
+	virtual void deleteMutex(MutexRef mutex) override;
+
+	virtual Audio::Mixer *getMixer() override;
+
+	virtual void quit() override;
+
+	virtual void fatalError() override;
+
+	virtual void displayMessageOnOSD(const Common::U32String &msg) override;
+
+	virtual void displayActivityIconOnOSD(const Graphics::Surface *icon) override;
+
+	virtual void logMessage(LogMessageType::Type type, const char *message) override;
+
+	virtual void messageBox(LogMessageType::Type type, const char *message) override;
+
+	virtual bool isConnectionLimited() override;
+};
+
 #endif
